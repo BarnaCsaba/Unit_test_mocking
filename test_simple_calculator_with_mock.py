5c912c9afccb96bdff8c53ecd2735a06dcedb72c
Barna Csaba <barna.csaba@diak.szbi-pg.hu>
2026-01-05 12:07:00 +0100

    k

--- test_simple_calculator_with_mock.py
+++ test_simple_calculator_with_mock.py
@@ -1,6 +1,15 @@
-def test_add_with_mock_history_service():
-    pass
+import pytest
+from simple_calculator import SimpleCalculator
+from unittest.mock import Mock
 
+def test_add_with_mock_history_service():
+    mock_history = Mock()
+
+    calc = SimpleCalculator(history_service=mock_history)
+
+    result = calc.add(5, 3)
+
+    assert result == 8
 def test_multiply_with_multiple_mocks():
     pass
 
