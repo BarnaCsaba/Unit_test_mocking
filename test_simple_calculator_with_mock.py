86e23bd9fb2a029333fecb0e1d1a9b11750e26be
Barna Csaba <barna.csaba@diak.szbi-pg.hu>
2026-01-05 12:18:54 +0100

    a

--- test_simple_calculator_with_mock.py
+++ test_simple_calculator_with_mock.py
@@ -4,14 +4,23 @@ from unittest.mock import Mock
 
 def test_add_with_mock_history_service():
     mock_history = Mock()
+    
 
     calc = SimpleCalculator(history_service=mock_history)
 
     result = calc.add(5, 3)
 
     assert result == 8
-def test_multiply_with_multiple_mocks():
-    pass
 
+    mock_history.save_operation.assert_called_once()
+    mock_history.save_operation.assert_called_with("add", 5, 3, 8)
+def test_multiply_with_multiple_mocks():
+    mock_history = Mock()
+    mock_logger = Mock()
+
+    mock_history.save_operation.assert_called_once()
+    mock_history.save_operation.assert_called_with("add", 5, 3, 8)
+    mock_logger.save_operation.assert_called_once()
+    mock_logger.save_operation.assert_called_with("add", 5, 3, 8)
 def test_get_history_count_with_mock():
     pass
